cee004a64897b71ed1ec1ef52d1925de78734ae8
memovalverd <guillermovalverde42@gmail.com>
2022-12-23 00:09:33 -0600

    forms and email service

---
+++ blog_application/mysite/blog/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+
+class EmailPostForm(forms.Form):
+    name     = forms.CharField(max_length=25)
+    email    = forms.EmailField(required=True)
+    to       = forms.EmailField(required=True)
+    comments = forms.CharField(required=False, widget=forms.Textarea)
+
+
--- blog_application/mysite/mysite/settings.py
+++ blog_application/mysite/mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,9 +28,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -122,3 +121,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Email server configuration
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'guillermovalverde42@gmail.com'
+EMAIL_HOST_PASSWORD = config('GOOGLE_PASS_SMTP')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
